2cc366122c344b0e3c41c2df1b44a481b88fa597
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-10-17 10:21:43 -0300

    feature(feed): Thread Cell

--- ThreadsCopy/Core/Feed/FeedView.swift
+++ ThreadsCopy/Core/Feed/FeedView.swift
@@ -9,7 +9,35 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    
+                    ForEach(0...10, id: \.self) { thread in
+                        ThreadCell()
+                    }
+                }
+            }
+            .refreshable {
+                print("DEBUG: refresh Threads")
+            }
+            .navigationTitle("Threads")
+            .navigationBarTitleDisplayMode(.inline)
+            
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        print("DEBUG: refresh Threads")
+                    } label: {
+                        Image(systemName: "arrow.counterclockwise")
+                            .tint(.black)
+                    }
+                }
+            }
+            
+        }
+
     }
 }
 
---
+++ ThreadsCopy/Core/Feed/ThreadCell.swift
@@ -0,0 +1,98 @@
+//
+//  ThreadCell.swift
+//  ThreadsCopy
+//
+//  Created by Lucas Migge on 17/10/23.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            
+            HStack(alignment: .top, spacing: 12) {
+            
+                Image("userLucas")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    
+                    HStack {
+                        Text("miggelucas")
+                            .foregroundStyle(.primary)
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            
+                        
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundStyle(.tertiary)
+                        
+                        
+                        Button {
+                            
+                            
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundStyle(.secondary)
+                        }
+                        .buttonStyle(.plain)
+                    
+                    }
+                    
+                   
+                    
+                    Text("Developing in iOS can be fun")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 12) {
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                        
+                    }
+                    .padding(.vertical, 8)
+                    .tint(.black)
+                    
+                    
+                }
+            }
+            
+            Divider()
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 4)
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
