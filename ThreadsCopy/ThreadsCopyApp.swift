cba08ffd030e6aeec7206cf843a3d99cc8be8442
Lucas Migge <53379393+miggelucas@users.noreply.github.com>
2023-10-16 23:19:33 -0300

    Initial Commit

---
+++ ThreadsCopy/ThreadsCopyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCopyApp.swift
+//  ThreadsCopy
+//
+//  Created by Lucas Migge on 16/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCopyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
